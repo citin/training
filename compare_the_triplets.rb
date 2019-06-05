8b4b650eb698444c8029515754a75e87c4c474c5
Andres Bernardi <cito.bernardi@gmail.com>
2019-06-05 19:51:01 +0200

    adds compare the triplets

---
+++ compare_the_triplets.rb
@@ -0,0 +1,7 @@
+# Complete the compareTriplets function below.
+def compareTriplets(a, b)
+  result = [0, 0]
+  a.zip(b)
+    .each {|first, second|result[((first > second) ? 0 : 1)] += 1 if first != second }
+  result
+end
